583929669de02d18679b5b94c228a4534c8532e4
premthebuilder <talk2premchand@gmail.com>
2018-02-11 15:04:50 -0500

    installed django-phone-login

--- ww/ww/settings.py
+++ ww/ww/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users_app',
+    'phone_login',
     'rest_framework',
     'rest_framework.authtoken',
 ]
